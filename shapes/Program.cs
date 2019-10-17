7e0d77c885acdc5b55e2206755450de516470953
shubham710176 <55733768+shubham710176@users.noreply.github.com>
2019-10-17 16:39:42 +0530

    Add files via upload

---
+++ shapes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          
+
+            int choice;
+            Console.WriteLine("enter the choice for shape:");
+            choice = int.Parse(Console.ReadLine());
+            
+            switch (choice)
+            {
+                case 1:
+
+                    
+                    Console.WriteLine("shape is cuboid");
+                    shape1 s = new shape1();
+                    Console.WriteLine("and volume is:" + s.getvolume());
+                    break;
+                case 2:
+                    
+                    Console.WriteLine("shape is rectangle");
+                    shape1 c = new shape1();
+                    Console.WriteLine("and area is:" + c.getrectarea());
+                    break;
+                case 3:
+                    Console.WriteLine("shape is traingle");  
+                    shape1 d = new shape1();
+                    Console.WriteLine("and area is:" + d.gettrainglearea());
+                    break;
+                case 4:
+                    
+                    Console.WriteLine("shape is circle");
+                    shape1 t = new shape1();
+                    Console.WriteLine("and area is:" + t.getcirclearea());
+                    break;
+            }
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ shapes/shape1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes
+{
+     class  shape1
+    {
+        public double height;
+        public double length;
+        public double breadth;
+        public double radius;
+        public shape1()
+        {
+            this.height = height;
+            this.length = length;
+            this.breadth = breadth;
+            this.radius = radius;
+
+        }
+        public double getvolume()
+        {
+            length = double.Parse(Console.ReadLine());
+            breadth = double.Parse(Console.ReadLine());
+            height = double.Parse(Console.ReadLine());
+            return length * breadth * height;
+        }
+        public double getrectarea()
+        {
+            length = double.Parse(Console.ReadLine());
+            breadth = double.Parse(Console.ReadLine());
+            return length * breadth;
+        }
+        public double gettrainglearea()
+        {
+            breadth = double.Parse(Console.ReadLine());
+            height = double.Parse(Console.ReadLine());
+            return 0.5 *breadth* height;
+        }
+        public double getcirclearea()
+        {
+            radius= double.Parse(Console.ReadLine());
+            return 3.14 * radius * radius;
+        }
+    }
+}
