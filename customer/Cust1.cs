4ce0918a6663a19c1962091c1b3e1a8eee47729a
shubham710176 <55733768+shubham710176@users.noreply.github.com>
2019-10-18 00:53:19 +0530

    Add files via upload

---
+++ customer/Cust1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customer
+{
+    class Cust1
+    {
+        public int customer_id;
+        public string customer_name;
+        public int account_no;
+        public double opening_balance;
+
+        public  Cust1(int customer_id, string customer_name, int account_no, double opening_balance)
+        {
+            //    Console.WriteLine("enter id:");
+            //    customer_id =int.Parse(Console.ReadLine());
+            //    Console.WriteLine("enter name :");
+            //    customer_name = Console.ReadLine();
+            //    Console.WriteLine("enter account no.");
+            //    account_no = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("enter opening balance");
+            //    opening_balance = double.Parse(Console.ReadLine());
+            this.customer_id = customer_id;
+            this.customer_name = customer_name;
+            this.account_no = account_no;
+            this.opening_balance = opening_balance;
+        }
+        public void  Withdraw(double x)
+        {
+            opening_balance=(opening_balance - x);
+            Console.WriteLine("current balance is:" + opening_balance);
+        }
+        public void  Deposit(double y)
+        {
+            opening_balance = (opening_balance + y);
+            Console.WriteLine("current balance is:" + opening_balance);
+        }
+        //public void displaydet()
+        //{
+        //    Console.WriteLine("the id is:" + customer_id);
+        //    Console.WriteLine("the name is:" + customer_name);
+        //    Console.WriteLine("the account_no is:" + account_no);
+        //    Console.WriteLine("the account balance is:" + opening_balance);
+        //}
+    }
+}
---
+++ customer/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customer
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Cust1 c = new Cust1(121,"cus1",4563,34567);
+            Cust1 c1 = new Cust1(247, "cus2", 4567, 33547);
+            Cust1 c2 = new Cust1(318, "cus3", 4575, 34490);
+            int choice;
+            Console.WriteLine("enter customer no.");
+            choice= int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Customer details are:");
+                    Console.WriteLine("id is"+c.customer_id);
+                    Console.WriteLine("name is"+c.customer_name);
+                    Console.WriteLine("account no."+c.account_no);
+                    Console.WriteLine("opening balance."+c.opening_balance);
+                    Withdra:
+                    Console.WriteLine("Do you wish to withdraw amount");
+                    Console.WriteLine("press y for continue and n to go to next option");
+                    string ch;
+                    ch = Console.ReadLine();
+                    if (ch == "y")
+                    {
+                        Console.WriteLine("enter withdraw amount");
+                        double withdraw;
+                        withdraw = double.Parse(Console.ReadLine());
+                        if (withdraw < (c.opening_balance))
+                        {
+                            c.Withdraw(withdraw);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Withdrawal cant take place");
+                        }
+                    }
+                    else
+                    {
+                        goto deposit;
+                    }
+                    deposit:
+                    Console.WriteLine("Do you wish to deposit amount");
+
+                    Console.WriteLine("press y for continue and n to go to next account");
+                    string ch1;
+                    ch1 = Console.ReadLine();
+                    if (ch1 == "y")
+                    {
+                        Console.WriteLine("enter deposit amount");
+                        double deposit;
+                        deposit = double.Parse(Console.ReadLine());
+                        c.Deposit(deposit);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Do you wish to transact one more time");
+                        Console.WriteLine("press y for continue and n to go to next account");
+                        string ch2;
+                        ch2 = Console.ReadLine();
+                        if (ch2 == "y")
+                        {
+                            goto Withdra;
+                        }
+                 
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Customer details are:");
+                    Console.WriteLine("id is" + c1.customer_id);
+                    Console.WriteLine("name is" + c1.customer_name);
+                    Console.WriteLine("account no." + c1.account_no);
+                    Console.WriteLine("opening balance." + c1.opening_balance);
+                Withdrac1:
+                    Console.WriteLine("Do you wish to withdraw amount");
+                    Console.WriteLine("press y for continue and n to go to next option");
+                    string chc1;
+                    chc1 = Console.ReadLine();
+                    if (chc1 == "y")
+                    {
+                        Console.WriteLine("enter withdraw amount");
+                        double withdraw;
+                        withdraw = double.Parse(Console.ReadLine());
+                        if (withdraw < (c1.opening_balance))
+                        {
+                            c.Withdraw(withdraw);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Withdrawal cant take place");
+                        }
+                    }
+                    else
+                    {
+                        goto depositc1;
+                    }
+                depositc1:
+                    Console.WriteLine("Do you wish to deposit amount");
+
+                    Console.WriteLine("press y for continue and n to go to next account");
+                    string chc11;
+                    chc11 = Console.ReadLine();
+                    if (chc11 == "y")
+                    {
+                        Console.WriteLine("enter deposit amount");
+                        double deposit;
+                        deposit = double.Parse(Console.ReadLine());
+                        c1.Deposit(deposit);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Do you wish to transact one more time");
+                        Console.WriteLine("press y for continue and n to go to next account");
+                        string ch2;
+                        ch2 = Console.ReadLine();
+                        if (ch2 == "y")
+                        {
+                            goto Withdrac1;
+                        }
+
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Customer details are:");
+                    Console.WriteLine("id is" + c2.customer_id);
+                    Console.WriteLine("name is" + c2.customer_name);
+                    Console.WriteLine("account no." + c2.account_no);
+                    Console.WriteLine("opening balance." + c2.opening_balance);
+                Withdrac2:
+                    Console.WriteLine("Do you wish to withdraw amount");
+                    Console.WriteLine("press y for continue and n to go to next option");
+                    string chc2;
+                    chc2 = Console.ReadLine();
+                    if (chc2 == "y")
+                    {
+                        Console.WriteLine("enter withdraw amount");
+                        double withdraw;
+                        withdraw = double.Parse(Console.ReadLine());
+                        if (withdraw < (c2.opening_balance))
+                        {
+                            c2.Withdraw(withdraw);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Withdrawal cant take place");
+                        }
+                    }
+                    else
+                    {
+                        goto depositc2;
+                    }
+                depositc2:
+                    Console.WriteLine("Do you wish to deposit amount");
+
+                    Console.WriteLine("press y for continue and n to go to next account");
+                    string chc21;
+                    chc21 = Console.ReadLine();
+                    if (chc21 == "y")
+                    {
+                        Console.WriteLine("enter deposit amount");
+                        double deposit;
+                        deposit = double.Parse(Console.ReadLine());
+                        c1.Deposit(deposit);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Do you wish to transact one more time");
+                        Console.WriteLine("press y for continue and n to go to next account");
+                        string ch2;
+                        ch2 = Console.ReadLine();
+                        if (ch2 == "y")
+                        {
+                            goto Withdrac2;
+                        }
+
+                    }
+                    break;
+            }
+
+        }
+    }
+}
