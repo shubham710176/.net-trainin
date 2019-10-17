c1012c3175d74c21a2092684cefcf82c0a796727
shubham710176 <55733768+shubham710176@users.noreply.github.com>
2019-10-17 16:38:00 +0530

    Add files via upload

---
+++ productdet/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productdet
+{
+    class Product
+    {
+        public string name;
+        public double price;
+        public double quantity;
+        public Product()
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        public double genbill()
+        {
+            return price * quantity;
+        }
+    }
+}
---
+++ productdet/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productdet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("ENTER THE ITEM YOU WANT TO BUY");
+            int choice;
+            
+            choice = int.Parse(Console.ReadLine());
+            Product p = new Product();
+            switch (choice)
+            {
+                case 1:
+                    p.name = "nike";
+                    p.price = 2000;
+                    p.quantity = 20;
+                    Console.WriteLine("this is nike shoes:");
+                    
+                    Console.WriteLine("the bill is"+p.genbill());
+                    break;
+                case 2:
+                    p.name = "puma";
+                    p.price = 1000;
+                    p.quantity = 20;
+                    Console.WriteLine("this is puma shoes:");
+                    
+                    Console.WriteLine("the bill is" + p.genbill());
+                    break;
+                case 3:
+                    p.name = "adidas";
+                    p.price = 1500;
+                    p.quantity = 20;
+                    Console.WriteLine("this is adidas shoes:");
+                    
+                    Console.WriteLine("the bill is" + p.genbill());
+                    break;
+           }
+            Console.WriteLine("do you want to add more items");
+            string ch = Console.ReadLine();
+                if (ch == "y") 
+            {
+                Console.WriteLine("ENTER THE ITEM YOU WANT TO BUY");
+                goto Newitem;
+
+            }
+            else {
+                Console.WriteLine("total price" + p.genbill());
+            }
+
+
+            Newitem:
+            int choice1;
+
+            choice1 = int.Parse(Console.ReadLine());
+            switch (choice1)
+                {
+                    case 1:
+                        p.name = "nike";
+                        p.price = 2000;
+                        p.quantity = 20;
+                        Console.WriteLine("this is nike shoes:");
+
+                        Console.WriteLine("the bill is" + p.genbill());
+                        break;
+                    case 2:
+                        p.name = "puma";
+                        p.price = 1000;
+                        p.quantity = 20;
+                        Console.WriteLine("this is puma shoes:");
+
+                        Console.WriteLine("the bill is" + p.genbill());
+                        break;
+                    case 3:
+                        p.name = "adidas";
+                        p.price = 1500;
+                        p.quantity = 20;
+                        Console.WriteLine("this is adidas shoes:");
+
+                        Console.WriteLine("the bill is" + p.genbill());
+                        break;
+                }
+                Console.WriteLine("do you want to add more items");
+                string ch1 = Console.ReadLine();
+                if (ch1 == "y")
+                {
+                    goto Newitem;
+
+                }
+                else
+                {
+                    Console.WriteLine("total price" + p.genbill());
+                }
+            Console.ReadKey();
+        }
+    }
+}
