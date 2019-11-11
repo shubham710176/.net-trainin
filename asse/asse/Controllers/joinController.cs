4c32a6c2ac2616df6edf0566e1e99500fbce14c6
shubham710176 <55733768+shubham710176@users.noreply.github.com>
2019-11-11 11:22:18 +0530

    ef assesment

---
+++ asse/asse/Controllers/joinController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.MobileControls;
+using asse.Models;
+
+namespace asse.Controllers
+{
+    public class joinController : Controller
+    {
+        // GET: join
+        public ActionResult Index()
+        {
+            dbfirstdemoEntities db = new dbfirstdemoEntities();
+            Employeedeparment emp = new Employeedeparment();
+             emp.employeedeparments = (from d in db.departments 
+                                      join e in db.Employees
+                             on d.ID equals e.ID
+                             select new Employeedeparment()
+                             {
+                                 EmployeeFName = e.FirstName,
+                                 EmployeelName = e.lastName,
+                                  DepartmentName = d.Name,
+                                 DepartmentLocation=d.location,
+                                 
+                               
+                             }).ToList();
+           
+           
+                
+            
+            return View(emp);
+        }
+
+        
+    }
+}
---
+++ asse/asse/Models/Employeedeparment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asse.Models
+{
+    public class Employeedeparment
+    {
+        public int ID { get; set; }
+        public string DepartmentName { get; set; }
+        public string DepartmentLocation { get; set; }
+        public string EmployeeFName { get; set; }
+        public string EmployeelName { get; set; }
+        public List<Employeedeparment> employeedeparments { get; set; }
+     
+    }
+    
+}
